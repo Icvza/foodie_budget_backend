3558de5a799c55767689d7b26786125a11957be9
Isaac <devisaacalejandro@gmail.com>
2022-01-30 18:14:28 -0500

    Application helpers defined

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,35 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
-
-  # GET /users
+    
   def index
     @users = User.all
-      if @users
-        render json: {
-        users: @users
-      }
-      else
-        render json: {
-        status: 500,
-        errors: ['no users found']
-      }
+    if @users
+      render json: {
+      users: @users
+    }
+    else
+      render json: {
+      status: 500,
+      errors: ['no users found']
+    }
     end
   end
 
-  # GET /users/1
   def show
-    render json: @user
+    @user = User.find(params[:id])
+    if @user
+      render json: {
+      user: @user
+    }
+    else
+      render json: {
+      status: 500,
+      errors: ['user not found']
+    }
+    end
   end
-
-  # POST /users
+    
   def create
-    @user = User.new(user_params)
-
+    user = User.new(user_params)
     if @user.save
       login!  
       render json: {
@@ -35,32 +40,13 @@ class UsersController < ApplicationController
       render json: {
       status: 500,
       errors: @user.errors.full_messages
-  }
+    }
     end
   end
 
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
 
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
+private
     def user_params
       params.require(:user).permit(:username, :firstname, :lastname, :password, :password_confirmation, :email)
     end
-end
+  end
